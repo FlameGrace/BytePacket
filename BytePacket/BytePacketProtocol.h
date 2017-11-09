379344cea8f9733372e71af87ddfecfdb616510a
FlameGrace <flamegrace@hotmail.com>
2017-11-09 15:51:40 +0800

    add comment

--- BytePacket/BytePacketProtocol.h
+++ BytePacket/BytePacketProtocol.h
@@ -21,7 +21,11 @@ typedef NS_ENUM(NSInteger, BytePacketErrorCode){
 
 //编码后的数据或需要解码的数据
 @property (strong ,nonatomic) NSData *encodeData;
-//编码后的数据长度或一个完整的可解码块的数据长度（在解码成功后必须计算并赋值）
+/*
+ 编码后的数据长度或一个完整的可解码块的数据长度（在解码成功后必须计算并赋值,否则会影响到Decoder循环解包）
+ The length of the data after be encoded or the length of a full data packet which can be decoded.
+ (It must be assigned after a full data packet had be decode.Otherwise, it will affect the decoder which doing cycle decode)
+*/
 @property (assign, nonatomic) NSUInteger encodeLength;
 
 //解码
